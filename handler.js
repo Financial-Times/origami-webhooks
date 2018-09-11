6a67843c45a4ad83573af578102b6728226c78df
Gabrielle von Koss <gabivk@gmail.com>
2018-09-11 15:33:00 +0100

    linting

--- handler.js
+++ handler.js
@@ -1,22 +1,27 @@
-const verifyGithubWebhook = require('./lib/verify-github-webhook.js');
+'use strict';
+const fetch = require('node-fetch');
+
 const env = require('./env.js')();
 const errorObject = require('./lib/helpers').responseObject;
+const verifyGithubWebhook = require('./lib/verify-github-webhook.js');
 
-module.exports.webhooks = (event, context, callback) => {
+module.exports.webhooks = (event) => {
 	verifyGithubWebhook(event);
 
 	let payload;
-	try  {
+	try {
 		payload = JSON.parse(event.body);
 	} catch (err) {
-		return errorObject(err, 400)
+		return errorObject(err, 400);
 	}
 
+	/* eslint-disable quotes */
 	const content = {
-		text: `${payload.issue.user.login} ${payload.action} an issue on ${payload.repository.name}.\n<${payload.issue.html_url}|*Title: ${payload.issue.title}*>\n${payload.issue.body ? payload.issue.body : ''}`,
-		mrkdwn: true,
-		icon_emoji: ':origami-crane:'
-	}
+		text: `${payload.issue.user.login} ${payload.action} an issue on ${payload.repository.name}.\n` +
+		`<${payload.issue.html_url}|*Title: ${payload.issue.title}*>\n${payload.issue.body ? payload.issue.body : ''}` ,
+		mrkdwn: true
+	};
+	/* eslint-enable quotes */
 
 	const sendPayload = async (content) => {
 		return await fetch(env.SLACK_WEBHOOK_URL, {
@@ -25,16 +30,16 @@ module.exports.webhooks = (event, context, callback) => {
 				'Content-type': 'application/json'
 			},
 			body: JSON.stringify(content)
-		})
-	}
+		});
+	};
 
 	return sendPayload(content)
 		.then(res => {
 			return {
 				statusCode: res.statusCode
-			}
+			};
 		})
 		.catch(err => {
 			return errorObject(err, 500);
-		})
+		});
 };
--- test/unit/lib/helpers.test.js
+++ test/unit/lib/helpers.test.js
@@ -4,7 +4,7 @@ const proclaim = require('proclaim');
 const helpers = require('../../lib/helpers.js');
 
 describe('responseObject', () => {
-	let responseObject = helpers.responseObject;
+	const responseObject = helpers.responseObject;
 	let mockBody;
 	let mockObject;
 
@@ -21,10 +21,10 @@ describe('responseObject', () => {
 		proclaim.equal(mockObject.body, JSON.stringify(mockBody));
 	});
 
-})
+});
 
 describe('sha1', () => {
-	let sha1 = helpers.sha1;
+	const sha1 = helpers.sha1;
 
 	it('is a helper function', () => {
 		proclaim.isFunction(sha1);
@@ -33,6 +33,6 @@ describe('sha1', () => {
 	it('returns a signed hash with a key and with body content', () => {
 		const body = 'This is the body';
 		const key = 'key';
-		proclaim.deepStrictEqual(sha1(key, body), 'sha1=00f4245c843497dc11c084e880cc285007e9163e')
+		proclaim.deepStrictEqual(sha1(key, body), 'sha1=00f4245c843497dc11c084e880cc285007e9163e');
 	});
-})
+});
