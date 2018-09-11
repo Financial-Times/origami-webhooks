362071e65f212c1c8daa581c248772ba8f816054
Gabrielle von Koss <gabivk@gmail.com>
2018-09-11 12:48:46 +0100

    break out some logic

--- handler.js
+++ handler.js
@@ -1,54 +1,15 @@
-const crypto = require('crypto');
-const fetch = require('node-fetch');
-
-function signRequestBody(key, body) {
-  return `sha1=${crypto.createHmac('sha1', key).update(body, 'utf-8').digest('hex')}`;
-}
-
+const verifyGithubWebhook = require('./lib/verify-github-webhook.js');
 const env = require('./env.js')();
+const errorObject = require('./lib/helpers').responseObject;
 
-module.exports.githubWebhookListener = (event, context, callback) => {
-  let errorMessage;
-  const token = env.GITHUB_WEBHOOK_SECRET;
-  const headers = event.headers;
-  const sig = headers['X-Hub-Signature'];
-  const githubEvent = headers['X-GitHub-Event'];
-  const id = headers['X-GitHub-Delivery'];
-  const calculatedSig = signRequestBody(token, event.body);
-
-	const errorObject = (message, statusCode) => {
-		return {
-			statusCode,
-			headers: { 'Content-Type': 'text/plain' },
-			body: message
-		}
-	}
-
-  if (typeof token !== 'string') {
-		return errorObject('Must provide a \'GITHUB_WEBHOOK_SECRET\' env variable', 401);
-	}
-
-  if (!sig) {
-    return errorObject('No X-Hub-Signature found on request', 401);
-  }
-
-  if (!githubEvent) {
-		return errorObject('No X-Github-Event found on request', 422);
-  }
-
-  if (!id) {
-		return errorObject('No X-Github-Delivery found on request', 401);
-  }
-
-  if (sig !== calculatedSig) {
-		return errorObject('X-Hub-Signature is incorrect. The GitHub webhook token doesn\'t match', 401);
-  }
+module.exports.webhooks = (event, context, callback) => {
+	verifyGithubWebhook(event);
 
 	let payload;
 	try  {
 		payload = JSON.parse(event.body);
 	} catch (err) {
-		return { err }
+		return errorObject(err, 400)
 	}
 
 	const content = {
