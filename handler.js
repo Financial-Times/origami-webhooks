765608b28acbc65e65c2feb3ee634fe07267970f
Gabrielle von Koss <gabivk@gmail.com>
2018-09-12 16:28:25 +0100

    add most basic handler for saving files to s3

--- handler.js
+++ handler.js
@@ -47,3 +47,24 @@ module.exports.webhooks = async (event) => {
 			throw httpError(err, 500);
 		});
 };
+
+const AWS = require('aws-sdk');
+
+const S3 = new AWS.S3({
+	accessKeyId: env.AWS_ACCESS_ID,
+	secretAccessKey: env.AWS_SECRET_ACCESS_KEY
+});
+
+module.exports.save = async (event) => {
+	try {
+		verifyGithubWebhook(event);
+	} catch (err) {
+		return err;
+	}
+
+	return await S3.putObject({
+		Bucket: env.BUCKET,
+		Key: 'test',
+		Body: event.body
+	}).promise()
+};
