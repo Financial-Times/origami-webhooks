862f2988b44226634138edb4eab5ea76f7d3f488
Gabrielle von Koss <gabivk@gmail.com>
2018-09-13 15:45:31 +0100

    break out and promisify

--- functions/save-to-s3.js
+++ functions/save-to-s3.js
@@ -1,52 +1,26 @@
 'use strict';
-const AWS = require('aws-sdk');
 
-const env = require('../env.js')();
-// const httpError = require('../lib/helpers').httpError;
+const s3 = require('../lib/s3-functions.js');
 const verifyGithubWebhook = require('../lib/verify-github-webhook.js');
 
 const listRepos = require('../lib/list-origami-repos');
 
-const s3 = new AWS.S3({
-	accessKeyId: env.AWS_ACCESS_ID,
-	secretAccessKey: env.AWS_SECRET_ACCESS_KEY
-});
-
-const checkObjectExists = (key) => {
-	const params = {
-		Bucket: env.BUCKET,
-		Key: key
-	};
-
-	return new Promise((resolve, reject) => {
-		s3.getObject(params, (err, data) => {
-			if (err) {
-				resolve(false);
-			}
-
-			resolve(data && data.Body ? JSON.parse(data.Body) : null );
-		});
-	});
-};
 
 exports.handler = async (event) => {
 	let payload;
 
-	//verify github webhook, and if all is well, return parsed event body
-	// try {
-	// 	payload = verifyGithubWebhook(event);
-	// } catch (err) {
-	// 	return err;
-	// }
+	try {
+		payload = verifyGithubWebhook(event);
+	} catch (err) {
+		return err;
+	}
 
-	payload = JSON.parse(event.body);
+	// payload = JSON.parse(event.body);
 
 	const whitelist = await listRepos();
 	const repository = payload.repository.name;
 
-
 	if (whitelist.includes(repository)) {
-		const object = await checkObjectExists(repository);
 		const body = {
 			issues: []
 		};
@@ -58,6 +32,7 @@ exports.handler = async (event) => {
 			url: payload.issue.html_url
 		};
 
+		const object = await s3.getS3Object(repository);
 		if (object) {
 			body.issues = object.issues;
 			body.issues.push(issue);
@@ -65,14 +40,7 @@ exports.handler = async (event) => {
 			body.issues = [issue];
 		}
 
-
-		//send to s3
-		return await s3.putObject({
-			Bucket: env.BUCKET,
-			Key: repository,
-			Body: JSON.stringify(body)
-		})
-		.promise();
+		return await s3.putS3Object(body, repository);
 	}
 
 	return Promise.resolve(`${repository} was not found in the Origami Registry`);
