a87860c365546f409c32d8908df13a2e0668b03e
Gabrielle von Koss <gabivk@gmail.com>
2018-09-21 11:44:27 +0100

    add slack function tests

---
+++ test/unit/lib/slack.test.js
@@ -0,0 +1,63 @@
+/* eslint-env mocha */
+'use strict';
+
+const fetchMock = require('fetch-mock');
+const proclaim = require('proclaim');
+const slack = require('../../../lib/slack.js');
+
+describe('slack', () => {
+	const mockObjects	 = [
+		{
+			repository: 'test',
+			action: 'opened',
+			url: '#',
+			title: 'issue 1'
+		},
+		{
+			repository: 'test',
+			action: 'opened',
+			url: '#',
+			title: 'issue 2'
+		},
+		{
+			repository: 'test',
+			action: 'closed',
+			url: '#',
+			title: 'issue 3'
+		}
+	];
+
+	const mockPayload = {
+		mrkdwn: true,
+		text: '*Issue activity since Thu morning*\n*Closed*\n_test_:\n— <#| issue 3>\n\n*Opened*\n_test_:\n— <#| issue 1>\n— <#| issue 2>\n\n'
+	};
+
+	afterEach(() => {
+		fetchMock.restore();
+	});
+
+	describe('.generatePayload', () => {
+		it('exports a function', () => {
+			proclaim.isFunction(slack.generatePayload);
+		});
+
+		it('generates a payload', () => {
+			const payload = slack.generatePayload(mockObjects, new Date('Fri Sep 21 2018'));
+			proclaim.deepStrictEqual(payload, mockPayload);
+		});
+	});
+
+	describe('.sendPayload', () => {
+		it('exports a functions', () => {
+			proclaim.isFunction(slack.sendPayload);
+		});
+
+		it('sends a payload', async () => {
+			const url = 'www.test.com';
+			fetchMock.post(url, 200);
+
+			await slack.sendPayload(url, mockPayload)
+				.then(res => proclaim.isTrue(res.ok));
+		});
+	});
+});
